0bb546b4a0c1d9da5a7bdca2cb9f3ef917401141
Ivan Stamenkovic <ivan@seemsindie.com>
2026-04-09 22:46:27 +0200

    Update formula to v0.3.1-beta.9 (drop x86_64-macos)

--- Formula/pidgn.rb
+++ Formula/pidgn.rb
@@ -1,26 +1,21 @@
 class Pidgn < Formula
   desc "CLI tool for the Pidgn web framework — project scaffolding, dev server, migrations, and more"
   homepage "https://github.com/seemsindie/pidgn_cli"
-  version "0.3.1-beta.4"
+  version "0.3.1-beta.9"
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/seemsindie/pidgn_cli/releases/download/v#{version}/pidgn-cli-v#{version}-aarch64-macos.tar.gz"
-      sha256 "3db9067a72171ae26290e5fdf340429f23db0c7de569086b538a04fb751faf73"
-    else
-      url "https://github.com/seemsindie/pidgn_cli/releases/download/v#{version}/pidgn-cli-v#{version}-x86_64-macos.tar.gz"
-      sha256 "fbe3712406b67279df42d637ca5c78c76733d7ae78aaab938161840cfa7248d0"
-    end
+    url "https://github.com/seemsindie/pidgn_cli/releases/download/v#{version}/pidgn-cli-v#{version}-aarch64-macos.tar.gz"
+    sha256 "98cacd642347f3db7d141268d222b2eea5b08dec9303f45c57172d73f2d4a5d6"
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/seemsindie/pidgn_cli/releases/download/v#{version}/pidgn-cli-v#{version}-aarch64-linux.tar.gz"
-      sha256 "f6781dd020ba2f16c8e392ccdda4bc1fc6cfb79dc333bbe14271eb884b77c823"
+      sha256 "58d118afd40115f26d65f945cf47f7c2554f60f623cb2757f59262441f173f78"
     else
       url "https://github.com/seemsindie/pidgn_cli/releases/download/v#{version}/pidgn-cli-v#{version}-x86_64-linux.tar.gz"
-      sha256 "9ba18de3221a4d30e2a52c1e3e2c260744235634d08bc7717e26f08ca3838ad1"
+      sha256 "edecde0a497801f940320051de99e9637afbc01a15d9c932aac61c322e83788b"
     end
   end
 
