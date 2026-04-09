0079ad6b85a760e8cbe3eb959a9e98b46aba2c60
Ivan Stamenkovic <ivan@seemsindie.com>
2026-04-10 01:14:17 +0200

    Update formula to v0.3.1-beta.12

--- Formula/pidgn.rb
+++ Formula/pidgn.rb
@@ -1,21 +1,21 @@
 class Pidgn < Formula
   desc "CLI tool for the Pidgn web framework — project scaffolding, dev server, migrations, and more"
   homepage "https://github.com/seemsindie/pidgn_cli"
-  version "0.3.1-beta.11"
+  version "0.3.1-beta.12"
   license "MIT"
 
   on_macos do
     url "https://github.com/seemsindie/pidgn_cli/releases/download/v#{version}/pidgn-cli-v#{version}-aarch64-macos.tar.gz"
-    sha256 "b8746c8da36d473cc4078e2ebc80287e7f81e5556e1048ada522fad4a3e886a4"
+    sha256 "1cd66c3f8bad4a2bddec4acf6d2166679858893699ffe4ed90a2c0fd27ac2caf"
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/seemsindie/pidgn_cli/releases/download/v#{version}/pidgn-cli-v#{version}-aarch64-linux.tar.gz"
-      sha256 "e772c057fbc8e2dcbfd06ae32ceac499861293ffb6e6c6f092469927f005dc26"
+      sha256 "477d84fd63856b84d53b604924c3b9d96bba07f9a8a5e76d410e87340b4cb955"
     else
       url "https://github.com/seemsindie/pidgn_cli/releases/download/v#{version}/pidgn-cli-v#{version}-x86_64-linux.tar.gz"
-      sha256 "b7388878b8a8768691c147e86697b6ff39760f5f33855bc41805f70c419dadfa"
+      sha256 "db72e28fb1af74faaadb06c9e99259122fc3949a7fce884c2f789ecc5548d792"
     end
   end
 
